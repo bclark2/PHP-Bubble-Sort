f1ae4ad7d78fc4d64a2fcfbbcc26f5d4dc68e74d
Bryan Clark <bclark2@umbc.edu>
2018-01-10 19:02:33 -0800

    Create bubbleSort.php

---
+++ bubbleSort.php
@@ -0,0 +1,18 @@
+function bubbleSort(array $array): array { 
+    $length = count($array); 
+
+    for ($i = 0; $i < $length; $i++) { 
+      for ($j = 0; $j < $length - 1; $j++) { 
+          if ($array[$j] > $array[$j + 1]) { 
+            $temp = $array[$j + 1]; 
+            $array[$j + 1] = $array[$j]; 
+            $array[$j] = $temp; 
+          } 
+      } 
+    }     
+    return $array; 
+}
+
+$arrayMixedNumbers = [-98, -78, 4567, 9023, 102332, 32, 10000000, 838, 0.0001, 0.01,3]; 
+$sortedArray       = bubbleSort($arrayMixedNumbers); 
+echo implode(", ", $sortedArray);
